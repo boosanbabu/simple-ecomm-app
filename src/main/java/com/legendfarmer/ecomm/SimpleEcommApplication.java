3c830ced1cd30a2d80d8e4bcfe6f968e7fd089f1
Boosan Babu M <boosan2020@gmail.com>
2018-11-05 17:02:28 +0530

    SpringBoot initial files

---
+++ src/main/java/com/legendfarmer/ecomm/SimpleEcommApplication.java
@@ -0,0 +1,12 @@
+package com.legendfarmer.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEcommApplication.class, args);
+	}
+}
